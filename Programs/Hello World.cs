e5e5d7153af027c4127fe4b612242099d8099072
Mahak Yadav <45141819+mahaky@users.noreply.github.com>
2020-09-04 17:59:10 +0530

    Create Hello World.cs

---
+++ Programs/Hello World.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programs
+{
+    class HelloWorld
+    {
+    //First hello to the world
+    
+        Console.WriteLine("Hello World!");
+    }
+}
