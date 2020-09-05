52d29c5835ffabb885ca3e590e8b1363a4767b3f
Mahak Yadav <45141819+mahaky@users.noreply.github.com>
2020-09-05 17:15:13 +0530

    Arrays

---
+++ Programs/Arrays.cs
@@ -0,0 +1,28 @@
+using System;
+
+
+namespace Collections
+{
+    public class Class1
+    {
+        static void Main(String[] args)
+        {
+        //Introduction to Arrays
+        //Printings days of week a
+            string[] daysofweek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            foreach(string day in daysofweek)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
